ef77083815770be776d0ce60b8fd0fd96e6a5c82
Rick Dutour Geerling <rick@trilon.io>
2023-07-19 15:38:46 +0200

    fix: swagger spec
    Mapped-types only inherit props that are decorated with @ApiProperty

--- apps/library/src/users/dto/base-user.dto.ts
+++ apps/library/src/users/dto/base-user.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsPositive } from 'class-validator';
 
 export class BaseUser {
   @IsInt()
   @IsPositive()
+  @ApiProperty({ type: 'number' })
   id: number;
 }
